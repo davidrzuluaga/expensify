f57decffbf6f6295e0433fd9e42278d8ece0cffe
DavidRZuluaga <git@davidrzuluaga.com>
2018-09-25 18:55:29 -0500

    months list

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -3,6 +3,15 @@ class ExpensesController < ApplicationController
     @tab = :expenses
     @type = Type.all
     @category = Category.all
-    @expense = Expense.all
+    @dates = []
+    
+    # if params[:question].present?
+    #   @questions = Question.where("title LIKE ? OR body LIKE ?", "%#{params[:question]}%", "%#{params[:question]}%")
+    # else
+    @expenses = Expense.order("date DESC")
+    # end
+    
+    @expenses.each {|exp| @dates << Date.new(exp.date.year, exp.date.month, 1)}
+    @months = @dates.uniq.sort
   end
 end
