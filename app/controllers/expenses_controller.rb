dd8d903200fee56fb15d80844c06a9e535a0d90d
DavidRZuluaga <git@davidrzuluaga.com>
2018-09-25 22:37:58 -0500

    fix filtros y category button v.5

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -5,21 +5,25 @@ class ExpensesController < ApplicationController
     @category = Category.all
     @dates = []
     @currentmonth = ""
+
+    Expense.all.each {|exp| @dates << Date.new(exp.date.year, exp.date.month, 1)}
+    @months = @dates.uniq.sort
     
     if params[:month].present?
       dateparams = params[:month].split(",")
       month = Date.new(dateparams[0].to_i, dateparams[1].to_i, dateparams[2].to_i)
       @currentmonth = month
-    end
-
-    if params[:category].present? || params[:type].present? || params[:month].present?
-      @expenses = Expense.where("category_id LIKE ? AND type_id LIKE ? AND date >= ? AND date <= ?", "%#{params[:category]}%", "%#{params[:type]}%", "#{month.at_beginning_of_month}", "#{month.at_end_of_month}").order("date DESC")
     else
-      @expenses = Expense.order("date DESC")
+      month = @months.first
+    end
+    
+    if params[:category].present? || params[:type].present? || params[:month].present?
+      @expenses = Expense.where("category_id LIKE ? AND type_id LIKE ? AND date >= ? AND date <= ?", "%#{params[:category]}%", "%#{params[:type]}%", "#{month.at_beginning_of_month}", "#{params[:month].present? ? month.at_end_of_month : Date.today.at_end_of_month}").order("date DESC")
+    else
+      @expenses = Expense.where("date >= ? AND date <= ?", Date.today.at_beginning_of_month, Date.today.at_end_of_month).order("date DESC")
+      @currentmonth = Date.today
     end 
-
-    @expenses.each {|exp| @dates << Date.new(exp.date.year, exp.date.month, 1)}
-    @months = @dates.uniq.sort
+    
     @total = @expenses.sum(:amount)
   end
 end
