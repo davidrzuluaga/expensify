7ace086df18fdfc997634027cf747b0838db5028
DavidRZuluaga <git@davidrzuluaga.com>
2018-09-25 21:32:45 -0500

    filters and Head Numbers

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,9 @@
 class DashboardController < ApplicationController
   def index
     @tab = :dashboard
+    @todayexpenses = Expense.where("DATE(date) = ?", Date.today).sum(:amount)
+    @yesterdayexpenses = Expense.where("DATE(date) = ?", Date.yesterday).sum(:amount)
+    @thismonthexpenses = Expense.where("date >= ? AND date <= ?", Date.today.at_beginning_of_month, Date.today.at_end_of_month).sum(:amount)
+    @lastmonthexpenses = Expense.where("date >= ? AND date <= ?", Date.today.at_beginning_of_month.last_month, Date.today.at_end_of_month.last_month).sum(:amount)
   end
 end
--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -4,14 +4,22 @@ class ExpensesController < ApplicationController
     @type = Type.all
     @category = Category.all
     @dates = []
+    @currentmonth = ""
     
-    # if params[:question].present?
-    #   @questions = Question.where("title LIKE ? OR body LIKE ?", "%#{params[:question]}%", "%#{params[:question]}%")
-    # else
-    @expenses = Expense.order("date DESC")
-    # end
-    
+    if params[:month].present?
+      dateparams = params[:month].split(",")
+      month = Date.new(dateparams[0].to_i, dateparams[1].to_i, dateparams[2].to_i)
+      @currentmonth = month
+    end
+
+    if params[:category].present? || params[:type].present? || params[:month].present?
+      @expenses = Expense.where("category_id LIKE ? AND type_id LIKE ? AND date >= ? AND date <= ?", "%#{params[:category]}%", "%#{params[:type]}%", "#{month.at_beginning_of_month}", "#{month.at_end_of_month}").order("date DESC")
+    else
+      @expenses = Expense.order("date DESC")
+    end 
+
     @expenses.each {|exp| @dates << Date.new(exp.date.year, exp.date.month, 1)}
     @months = @dates.uniq.sort
+    @total = @expenses.sum(:amount)
   end
 end
