e55d931fd8b1a45db7174312e406a46c679fe4a0
DavidRZuluaga <git@davidrzuluaga.com>
2018-11-01 21:45:05 -0500

    First Chart ready!

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -1,8 +1,16 @@
 class ChartsController < ApplicationController
+    def semester_type
+        @semester_type = []
+      Type.all.each do |ty|
+        @semester_type <<  {name: ty.name, data: Expense.where("date >= ? AND date <= ? AND type_id = #{ty.id}", 6.months.ago.at_beginning_of_month, Date.today.at_end_of_month).group_by_month(:date, format: "%m %y").sum(:amount) }
+      end
+      render json: @semester_type
+    end
+    
     def type
       @type = []
       Type.all.each do |ty|
-        @type <<  {name: ty.name, data: Expense.where("date >= ? AND date <= ? AND type_id = #{ty.id}", Date.today.at_beginning_of_month, Date.today).group_by_day_of_month(:date).sum(:amount) }
+        @type <<  {name: ty.name, data: Expense.where("date >= ? AND date <= ? AND type_id = #{ty.id}", Date.today.at_beginning_of_month,Date.today.at_end_of_month).group_by_day_of_month(:date).sum(:amount) }
       end
       render json: @type
     end
@@ -10,12 +18,12 @@ class ChartsController < ApplicationController
     def category
       @category = []
       Category.all.each do |cat|
-        @category << ["#{cat.name}", Expense.where("date >= ? AND date <= ? AND category_id = #{cat.id}", Date.today.at_beginning_of_month, Date.today).sum(:amount) ]
+        @category << ["#{cat.name}", Expense.where("date >= ? AND date <= ? AND category_id = #{cat.id}", Date.today.at_beginning_of_month,Date.today.at_end_of_month).sum(:amount) ]
       end
       render json: @category
     end
 
-    def accumulated  
+    def accumulated
       lastmonthexpaccumulated = {}
       lastmonthallexp = 0
       eachdaylastmonthexp = Expense.where("date >= ? AND date <= ?", Date.today.at_beginning_of_month.last_month, Date.today.at_end_of_month.last_month).group_by_day_of_month(:date).sum(:amount)
@@ -26,12 +34,12 @@ class ChartsController < ApplicationController
 
       thismonthexpaccumulated = {}
       thismonthallexp = 0
-      eachdaythismonthexp = Expense.where("date >= ? AND date <= ?", Date.today.at_beginning_of_month, Date.today).group_by_day_of_month(:date).sum(:amount)
+      eachdaythismonthexp = Expense.where("date >= ? AND date <= ?", Date.today.at_beginning_of_month,Date.today.at_end_of_month).group_by_day_of_month(:date).sum(:amount)
       eachdaythismonthexp.each do |day, exp|
         thismonthallexp += exp
         thismonthexpaccumulated[day] = thismonthallexp
       end
-      
+
        @twomonthsaccumulated = [{name: "This Month", data: thismonthexpaccumulated}, {name: "Last Month", data:lastmonthexpaccumulated}]
 
        render json: @twomonthsaccumulated
