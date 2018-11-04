279757e89e77be5a818d559db52d532746a6c728
DavidRZuluaga <git@davidrzuluaga.com>
2018-11-03 23:44:25 -0500

    All the API's CURL Set

--- app/controllers/api/v1/expenses_controller.rb
+++ app/controllers/api/v1/expenses_controller.rb
@@ -1,19 +1,46 @@
 class Api::V1::ExpensesController < ApplicationController
-    def index
-      @expenses = Expense.all
-      render json: @expenses
-    end
+  protect_from_forgery with: :null_session
+  before_action :set_expense, only: [:show, :update, :destroy]
 
-    def create
-      @expense = Expense.create(expense_params)   
-      if @expense.save
-        render json: @expense
-      else
-        render json: { errors: @expense.errors }, status: 422
-      end
-    end
+  def index
+    @expenses = Expense.all
+    render json: @expenses
+  end
 
-private
+  def show
+    render json: @expense
+  end
+
+  def create
+    @expense = Expense.create(expense_params)   
+    if @expense.save
+      render json: @expense
+    else
+      render json: { errors: @expense.errors }, status: 422
+    end
+  end
+
+  def update
+    @expense = Expense.find(params[:id])
+    if @expense.update(expense_params)
+      render json: @expense
+    else
+      render json: { errors: @expense.errors }, status: 422
+    end
+  end
+
+  def destroy
+    @expense.destroy
+    
+    head :no_content
+  end
+  
+  
+  private
+  
+    def set_expense
+      @expense = Expense.find(params[:id])    
+    end
 
     def expense_params
         params.require(:expense).permit(:date, :concept, :amount, :category_id, :type_id)
