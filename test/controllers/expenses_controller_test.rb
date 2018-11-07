90c626f19402f8763e8fb850fc38cd2c28054cbd
DavidRZuluaga <git@davidrzuluaga.com>
2018-11-06 22:52:01 -0500

    controllers tests

--- test/controllers/expenses_controller_test.rb
+++ test/controllers/expenses_controller_test.rb
@@ -1,7 +1,26 @@
 require 'test_helper'
 
 class ExpensesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
+  test "should show expenses index" do
+    get expenses_url
+    assert_response :success
+  end
+
+  # test "should create a expense" do
+  #   assert_difference('Expense.count') do
+  #     post expenses_path, params: { expense: { date: "2018-01-01", concept: "Example", amount: 1, category_id: Category.all.sample.id, type_id: Type.all.sample.id } }
+  #   end
+
+  #   assert_redirected_to expenses_url
   # end
+
+  # test "should destroy a expense" do
+  #   @expense = Expense.last
+  #   assert_difference('Expense.count', -1) do
+  #     delete expense_path(@expense)
+  #   end
+
+  #   assert_redirected_to expenses_url
+  # end
+  
 end
